da71acb7d64a15e900ed6590c17e68cac7cca4d3
Chris Gonzales <cm.gonzales@yahoo.com>
2017-08-29 19:56:14 -0700

    BufferedReader

--- src/server.java
+++ src/server.java
@@ -1,9 +1,12 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class server {
-
+	
 	
 	
 	public static void main(String[] args) {
@@ -11,8 +14,17 @@ public class server {
 		try {
 			ServerSocket ss = new ServerSocket(5800);
 			System.out.println("Waiting on Clients");
-			Socket s = ss.accept();
+			Socket soc = ss.accept();
 			System.out.println("Connection Established");
+			
+			
+			BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+			
+			PrintWriter out = new PrintWriter(soc.getOutputStream(), true);	
+			
+			String ans = in.readLine();
+					
+			
 			
 		} catch (IOException e) {
 			
