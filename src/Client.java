de789b3ee9a04536796b7b081f4d8fb136e5755b
Chris Gonzales <cm.gonzales@yahoo.com>
2017-09-04 14:08:57 -0700

    Added loops

--- src/Client.java
+++ src/Client.java
@@ -7,6 +7,7 @@ import java.net.Socket;
 public class Client {
 
 	public static void main(String[] args) throws Exception {
+		
 		Socket soc = new Socket("localhost", 5800);
 		
 		BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));     
@@ -18,16 +19,58 @@ public class Client {
  
         String input = "";
         
-        System.out.println("Name a place you visit where you arent allowed to touch anything?");
+        String response = "";
+        
+        String arr[] = {"museum", "hospital", "no"};
+        
+        System.out.println("Name a place you visit where you arent allowed to touch anything.");
         
         input = userInput.readLine();
-        out.println(input);
-        String response = in.readLine();
-        if (response.equals("museum")){
-        	System.out.println(10);
-        }else{
-            System.out.print("wrong");
         
+        out.println(input);
+        
+        response = in.readLine();
+        
+        if (response.equals(arr[0])){
+        
+        	System.out.println("Survey says " + 10);
+        
+        }else{
+           
+        	System.out.println("wrong");
+        
+	}
+        System.out.println("Name a Public Place Where You're Likely to Catch a Cold or Flu Bug.");
+        
+        input = userInput.readLine();
+        
+        out.println(input);
+        
+        response = in.readLine();
+        
+        if (response.equals(arr[1])){
+        
+        	System.out.println("Survey says  " + 10);
+        
+        }else{
+           
+        	System.out.println("wrong");
+}
+        System.out.println("Name a word that most people yell at their dogs.");
+        
+        input = userInput.readLine();
+        
+        out.println(input);
+        
+        response = in.readLine();
+        
+        if (response.equals(arr[2])){
+        
+        	System.out.println("Survey says  " + 10);
+        
+        }else{
+           
+        	System.out.println("wrong");
 	}
 }
 }
--- src/server.java
+++ src/server.java
@@ -1,20 +1,23 @@
 import java.io.BufferedReader;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+
 public class server {
 	
-	
-	
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 		
 		try {
 			ServerSocket ss = new ServerSocket(5800);
+			
 			System.out.println("Waiting on Clients");
+		    
 			Socket soc = ss.accept();
+			
 			System.out.println("Connection Established");
 			
 			
@@ -22,23 +25,22 @@ public class server {
 			
 			PrintWriter out = new PrintWriter(soc.getOutputStream(), true);	
 			
+			while(true){
+			
+			String answer[] = {"museum", "hospital", "no"}; 
+			
 			String ans = in.readLine();
 			
-			if(ans.equals("museum")){
-				
-				out.println("museum");
-			}else{
+		    for(int i = 0; i < answer.length; i++){
+		    	
+		    	if(ans.equals(answer[i])){
 			
-			for(int i = 0; i < 3; i++){
-				
-				out.println("try again");
-			}
-			}
+			       out.println(answer[i]);
 	
 			
-			
-					
-			
+			}
+		    }
+			}
 			
 		} catch (IOException e) {
 			
